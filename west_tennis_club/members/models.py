9eba96b71e944ad1191f7397d89305f6bdb06254
HeisenDelta <ariya8.narayan7@outlook.jp>
2024-06-26 11:20:27 +0900

    added existing project

---
+++ west_tennis_club/members/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    birthdate = models.DateField(null=True)
+    picture_url = models.CharField(max_length=255, default='#')
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
